866e408cada36647e5cbb9e32ed1c500530a64c5
jonathanapm <378623#Japm930522>
2021-03-30 10:31:07 -0300

    Create department and program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,13 @@
-package application;public class Program {
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department department = new Department(1, "Books");
+
+        System.out.println(department);
+    }
 }
